b7db0a64f75a7734f6e0b49a969ec610c4911552
Cristobal Salazar <chris.salza@gmail.com>
2020-12-17 22:05:04 -0500

    rm writableEnded return, add sendStatus for nums

--- src/components.ts
+++ src/components.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import { flatten } from './flatten'
-import * as Types from './app-types'
+import express from "express";
+import { flatten } from "./flatten";
+import * as Types from "./app-types";
 
-const DATA_KEY = '__component_data'
+const DATA_KEY = "__component_data";
 
 export function provider(props: any): express.Handler {
   return function (req, res, next) {
-    ;(req as any)[DATA_KEY] = props
-    next()
-  }
+    (req as any)[DATA_KEY] = props;
+    next();
+  };
 }
 
 export function pipe<T extends Types.Props = Types.Props>(
@@ -17,53 +17,49 @@ export function pipe<T extends Types.Props = Types.Props>(
 ) {
   return flatten(handler).map((componentHandler) =>
     toExpress(componentHandler, props)
-  )
+  );
 }
 
 function isCustomHandler(
   handler: Types.ComponentHandler | Types.TranslatedComponentHandler
 ): handler is Types.TranslatedComponentHandler {
-  return handler.__isComponent !== void 0
+  return handler.__isComponent !== void 0;
 }
 
 export function toExpress(
   handler: Types.ComponentHandler | Types.TranslatedComponentHandler,
   props: any
 ): Types.TranslatedComponentHandler {
-  if (isCustomHandler(handler)) return handler
+  if (isCustomHandler(handler)) return handler;
   const component: Types.TranslatedComponentHandler = async (
     req,
     res,
     next
   ) => {
-    let key = (req as any)[DATA_KEY]
-    Object.assign(key || {}, props)
+    let key = (req as any)[DATA_KEY];
+    Object.assign(key || {}, props);
     try {
-      const ret = await handler(Object.assign({ req, res }, key))
-      handleReturn(ret, res, next)
-      next()
+      const ret = await handler(Object.assign({ req, res }, key));
+      handleReturn(ret, res);
+      ret instanceof Error ? next(ret) : next();
     } catch (err) {
-      next(err)
+      next(err);
     }
-  }
-  component.__isComponent = true
-  return component
+  };
+  component.__isComponent = true;
+  return component;
 }
 
-export function handleReturn(
-  returnValue: any,
-  res: express.Response,
-  next: express.NextFunction
-) {
-  if (returnValue === null) return res.end()
-  else if (returnValue instanceof Error) return next(returnValue)
-  else if (res.writableEnded) return
+export function handleReturn(returnValue: any, res: express.Response) {
+  if (returnValue === null) return res.end();
   switch (typeof returnValue) {
-    case 'undefined':
-      return
-    case 'object':
-      return res.json(returnValue)
+    case "undefined":
+      return;
+    case "object":
+      return res.json(returnValue);
+    case "number":
+      return res.sendStatus(returnValue);
     default:
-      return res.send(returnValue)
+      return res.send(returnValue);
   }
 }
